f6fcd097ad53915a7a5ae3d02d9762741ef096e1
Alex <alexei.kadir@gmail.com>
2023-01-10 14:52:52 +0100

    Ajout d'un menu ouvrable

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 let movementbutton = document.getElementById("movement");
 let menu = document.getElementById("navmenu");
+let links = document.getElementById("navlinks");
 let menuopened = false;
 
 movementbutton.addEventListener("click", () => {
@@ -7,24 +8,29 @@ movementbutton.addEventListener("click", () => {
   });
 menu.addEventListener("click", () => {
     menuopened = !menuopened;
-    console.log("opened",menuopened);
-    if(menuopened) {
-        menu.className = "navmenucross";
-    } else {
-        menu.className = "";
+
+    if(menuopened){
+        links.style.right = "0";  
+        menu.className = "cross";
+}else{
+
+  links.style.right = "-110%";  
+  menu.className = "burger";
     }
-});
+      });
 
 window.onscroll = function () {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
     movementbutton.style.bottom = "20px";
-  } else {
+  else
     movementbutton.style.bottom = "-50px";
-  }
+
 };
 
-if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+
+
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
   movementbutton.style.bottom = "20px";
-} else {
+else
   movementbutton.style.bottom = "-50px";
-}
+
