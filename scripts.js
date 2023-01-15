643ca5dfe764d1bc6e88b7a0e6c83a199bd63620
Alex <alexei.kadir@gmail.com>
2023-01-15 18:21:43 +0100

    Ajout d'une page de contact

--- scripts.js
+++ scripts.js
@@ -1,36 +1,79 @@
-let movementbutton = document.getElementById("movement");
-let menu = document.getElementById("navmenu");
-let links = document.getElementById("navlinks");
-let menuopened = false;
+function init() {
+  // MOVEMENT BUTTON
 
-movementbutton.addEventListener("click", () => {
+  let movementbutton = document.getElementById("movement");
+
+  movementbutton.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
-menu.addEventListener("click", () => {
-    menuopened = !menuopened;
 
-    if(menuopened){
-        links.style.right = "0";  
-        menu.className = "cross";
-}else{
+  window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+      movementbutton.style.bottom = "20px";
+    else movementbutton.style.bottom = "-50px";
+  };
 
-  links.style.right = "-110%";  
-  menu.className = "burger";
-    }
-      });
-
-window.onscroll = function () {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
     movementbutton.style.bottom = "20px";
-  else
-    movementbutton.style.bottom = "-50px";
+  else movementbutton.style.bottom = "-50px";
 
-};
+  // BURGER MENU
 
+  let navmenu = document.getElementById("navmenu");
+  let navlinks = document.getElementById("navlinks");
+  let navmenuopened = false;
+  navmenu.addEventListener("click", () => {
+    navmenuopened = !navmenuopened;
 
+    if (navmenuopened) {
+      navlinks.style.right = "0";
+      navmenu.className = "cross";
+    } else {
+      navlinks.style.right = "-110%";
+      navmenu.className = "burger";
+    }
+  });
 
-if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
-  movementbutton.style.bottom = "20px";
-else
-  movementbutton.style.bottom = "-50px";
+  // LINKS
 
+  let linkname = document.body.getAttribute("data-linkname");
+  if (linkname != null) {
+    let links = document.getElementsByClassName("link_" + linkname);
+    for (let i = 0; i < links.length; i++) links[i].classList.add("active");
+  }
+
+  // PAGE
+
+  let pagename = document.body.getAttribute("data-pagename");
+  if (pagename != null) {
+    let page = document.getElementById("page");
+    page.textContent = pagename;
+  }
+}
+
+// AUTOLOADER
+
+let loaders = document.getElementsByClassName("loader");
+
+let toload = loaders.length;
+
+for (let i = 0; i < loaders.length; i++) {
+  let src = loaders[i].getAttribute("data-src");
+  if (src == null) {
+    toload--;
+    if (toload == 0) init();
+  } else {
+    let request = new XMLHttpRequest();
+
+    request.open("GET", src, true);
+
+    request.onload = function () {
+      if (request.status >= 200 && request.status < 400)
+        loaders[i].innerHTML = request.responseText;
+      toload--;
+      if (toload == 0) init();
+    };
+
+    request.send();
+  }
+}
